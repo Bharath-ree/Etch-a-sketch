23be71a15f2b5bf4c8d81aad56e322dbe55d32c3
vboxuser <vboxuser@Xubuntu.myguest.virtualbox.org>
2025-08-06 23:02:46 +0800

    added grid reset button and event listner

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ divs.forEach(function(square) {
         square.style.backgroundColor = 'black';
     });
 });
+
+//reset button and grid size
+function createGrid(size) {
+    container.innerHTML = ''; 
+    const totalSquares = size * size;
+    const squareSize = 640/size; 
+
+    for( let i = 0; i < totalSquares; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        
+        square.addEventListener('mouseover', function() {
+            square.style.backgroundColor = 'black';
+        });
+    }
+    container.appendChild(square);
+}
+
+//add event listener to reset button
+const resetBtn = document.querySelector('#resetBtn');
+resetBtn.addEventListener('click', function() {
+    let newSize = prompt('Enter new grid size (1-100):');
+    newSize = parseInt(newSize);
+    if (isNaN(newSize) || newSize < 1 || newSize > 100) {
+        alert('Please enter a valid number between 1 and 100.');
+        return;
+    }
+    createGrid(newSize);
+});
